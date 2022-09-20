ecba2d33ed136cf53c5276cf964834af0e1dd5a9
Gerald McCollam <gerald.mccollam@gmail.com>
2022-09-20 07:14:56 -0500

    Update impl.py

--- Program-HW1/strassen/impl.py
+++ Program-HW1/strassen/impl.py
@@ -4,7 +4,7 @@ from math import ceil, log
 import argparse, os, sys
 
 def matrix_product(A, B):
-  """FIXME
+  """ A brute force method of calculating the product of two matrices.
   """
   n = len(A)
   C = [[0 for i in range(n)] for j in range(n)]
@@ -13,3 +13,30 @@ def matrix_product(A, B):
           for j in range(n):
               C[i][j] += A[i][k] * B[k][j]
   return C
+
+def matrix_add(A, B):
+  """ Add two square matrices.
+  """
+n = len(A)
+C = [[0 for j in range(0, n)] for i in range(0, n)]
+for i in range(0, n):
+    for j in range(0, n):
+        C[i][j] = A[i][j] + B[i][j]
+return C
+
+
+def matrix_subtract(A, B):
+  """ Subtract two square matrices.
+  """
+n = len(A)
+C = [[0 for j in range(0, n)] for i in range(0, n)]
+for i in range(0, n):
+    for j in range(0, n):
+        C[i][j] = A[i][j] - B[i][j]
+return C
+
+
+def strassen():
+  """ An implementation of Strassen's Algorithm.
+  """
+    pass
