d879c4d4f4f7dc7f4b895a763d638ee68761006f
geraldmc <gerald.mccollam@gmail.com>
2022-10-15 08:59:25 -0500

    PP02

--- GMcC-PP02/testing/test_hashtables.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-from hashtables import SimpleHashTable
-
-BUCKET=100
-
-def test_should_not_contain_none_value_when_created():
-    assert None not in SimpleHashTable(capacity=BUCKET).values
