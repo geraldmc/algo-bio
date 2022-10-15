2b38ac75bef0ccbc00d476597ef28cf67bff6331
geraldmc <gerald.mccollam@gmail.com>
2022-10-15 08:39:03 -0500

    PP02

--- GMcC-PP02/driver.py
+++ GMcC-PP02/driver.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""driver.py: Main class for SHashing exercise
+"""driver.py
 """
+
+from filehandler.io import pre_process
 
 __author__      = "Gerald McCollam"
 __assignment__  = "Programming Problem 2"
@@ -12,26 +14,29 @@ __semester__    = "Fall, 2022"
 import argparse
 
 if __name__ == "__main__":
-  """ Driver main function for interacting with all modules. 
+  """ Main driver for interacting with other modules. 
   """
 
 import argparse
 
 def default():
-    # Support input and output w/ file.
-    inp = input("Enter input path: ")
-    outp = input("Enter output path: ")
-    return (inp, outp)
+  # Support file input.
+  try:
+    inp = input("Enter file input path: ")
+  except (FileNotFoundError, IsADirectoryError):
+    print('File not found.')
+  return (inp.strip())
 
-
-parser = argparse.ArgumentParser(description='Exercises with hashing.')
+parser = argparse.ArgumentParser(description='Exercise in hashing.')
 group = parser.add_mutually_exclusive_group(required=True)
 group.add_argument('-file', action="store_true",
-                    help='provide a path to a file containing one or \
-                    more paired matrices.')
+                    help='provide a path to a file containing hash input.')
 args = parser.parse_args()
 
 if args.file: # the default path for the assignment!
-    inp, outp = default()
+    inp = default()
+    raw_input = pre_process(inp)
+    for s in raw_input:
+      print (s)
 else:
-  pass
+  print('no other option yet...')
---
+++ GMcC-PP02/filehandler/io.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""io.py
+"""
+
+def pre_process(f):
+  """Preprocess input.
+  """
+  l2 = ''
+  try:
+    with open(f) as reader:
+      try:
+        contents = reader.read()
+        l1 = contents.split('\n\n')
+        l2 = [l.split('\n') for l in l1]
+      except (IOError, OSError):
+        print("Error reading from file")
+  except (FileNotFoundError, PermissionError, OSError):
+    print("Error opening file. Please try again.")
+  return l2[:-1] 
