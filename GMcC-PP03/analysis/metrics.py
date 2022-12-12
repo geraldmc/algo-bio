9ea11def048b2a9a93403e37f27700f666b35e88
Gerald McCollam <gerald.mccollam@gmail.com>
2022-12-12 14:04:26 -0600

    Update metrics.py

--- GMcC-PP03/analysis/metrics.py
+++ GMcC-PP03/analysis/metrics.py
@@ -5,15 +5,14 @@ from random import choice
 
 # https://stackoverflow.com/questions/21205836/generating-random-sequences-of-dna
 
-def String(length):
+def dna_string(length):
 	DNA=""
 	for count in range(length):
 		DNA+=choice("CGTA")
 	return DNA
 
-# call weightedchoice instead of choice in the loop:
-# DNA+=weightedchoice([("C", 10], ("G", 20), ("A", 40"), ("T", 30)])
-
-DNA+=weightedchoice([("C", 10], ("G", 20), ("A", 40"), ("T", 30)])
 def weightedchoice(items): # this doesn't require the numbers to add up to 100
 	return choice("".join(x * y for x, y in items))
+
+# call weightedchoice instead of choice in the loop:
+#DNA+=weightedchoice([("C", 10], ("G", 20), ("A", 40"), ("T", 30)])
