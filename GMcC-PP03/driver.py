acc28cc4b4b37c15bc0d1a4edc0502e711115768
geraldmc <gerald.mccollam@gmail.com>
2022-12-17 23:44:23 -0600

    rearranges

--- GMcC-PP03/driver.py
+++ GMcC-PP03/driver.py
@@ -43,24 +43,39 @@ if __name__ == "__main__":
     sequences.append(seq_input[k])
 
 # Run the base DNA string comparison from the supplied input file --------------
-# Compare every element (sequence) to every other element (sequence), only once. 
+# Compare every element (sequence) to every other element (sequence), once. 
 # Compare order (0,1), (0,2), (0,3), (1,2), (1,3), (2,3)
-#  for s1, s2 in itertools.combinations(sequences, 2):
-#    LCS2(s1, s2)
+  print('Running LCS2 on provided data...')
+  for s1, s2 in itertools.combinations(sequences, 2):
+    LCS2(s1, s2)
+  print('Running LCS3 on provided data...')
+  for s1, s2 in itertools.combinations(sequences, 2):
+    LCS3(s1, s2)
+  print('-----------------------------------------------------------')
 
 if RUN_ALL:
-  # Run generated DNA string comparisons ---------------------------------------
-  # https://codereview.stackexchange.com/questions/165245/plot-timings-for-a-range-of-inputs
-  # NOTE: Don't try recursive solution here (no LCS1)!
-    #idx1 = [2, 6, 10, 14, 18, 22, 26, 30, 34, 38, 42, 46, 50, 54, 58] # list(np.arange(2, 60, 4))
-    lcs1_only = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 19, 20]
+  # Run generated DNA string comparisons
+    idx1 = [5, 6, 7, 8] #,9,10,11,12,13,14,15,16,17,19,20]
+    idx2 = [2728,3004,3280,3556] #,3832,4108,4384,4660,4936,5212,5488,5764,6040,6316,6592]
 
-    #idx1 = [200, 476, 752, 1028, 1304, 1580, 1856, 2132, 2408, 2684, 2960]
-    #idx1 =  [2728,3004,3280,3556,3832,4108,4384,4660,4936,5212,5488,5764,6040,6316,6592]
-    #idx2 =  [2980, 5959, 8938, 11916, 14895, 17874, 20853, 23831, 26810, 29789, 32768]
-    #idx3 =  [11375, 16791, 22207, 27623, 33039, 38455, 43871, 49287, 54703, 60119, 65536]
-    
+    print('Running LCS1 on generated data...')
     for i in idx1:
-        S1 = dna_str(i)
+        S1 = dna_str(i) # generate a random ACTG string of length i.
         S2 = dna_str(i)
         LCS1(S1, S2)
+
+    print('Running LCS2 on generated data...')
+    for i in idx2:
+        S1 = dna_str(i) # generate a random ACTG string of length i.
+        S2 = dna_str(i)
+        LCS2(S1, S2)
+    
+    print('Running LCS3 on generated data...')
+    for i in idx2:
+        S1 = dna_str(i)
+        S2 = dna_str(i)
+        LCS3(S1, S2)
+  
+    print('-------------------------fin-------------------------------')
+    print()
+
