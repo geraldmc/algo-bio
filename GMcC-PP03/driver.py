d7e2162f38216528bf642fa909db7f899c4aa3b6
geraldmc <gerald.mccollam@gmail.com>
2022-12-18 12:42:36 -0600

    rearranges

--- GMcC-PP03/driver.py
+++ GMcC-PP03/driver.py
@@ -45,17 +45,22 @@ if __name__ == "__main__":
 # Run the base DNA string comparison from the supplied input file --------------
 # Compare every element (sequence) to every other element (sequence), once. 
 # Compare order (0,1), (0,2), (0,3), (1,2), (1,3), (2,3)
+  print('The recursive LCS1 is not run on provided data... (too slow!)')
+  print()
+  time.sleep(2)  
   print('Running LCS2 on provided data...')
   for s1, s2 in itertools.combinations(sequences, 2):
     LCS2(s1, s2)
   print('Running LCS3 on provided data...')
   for s1, s2 in itertools.combinations(sequences, 2):
     LCS3(s1, s2)
-  print('-----------------------------------------------------------')
+  print()
+  print('------------------------generated---------------------------')
+  print()
 
 if RUN_ALL:
   # Run generated DNA string comparisons
-    idx1 = [5, 6, 7, 8] #,9,10,11,12,13,14,15,16,17,19,20]
+    idx1 = [5, 6, 7, 8, 9] #,10,11,12,13,14,15,16,17,19,20]
     idx2 = [2728,3004,3280,3556] #,3832,4108,4384,4660,4936,5212,5488,5764,6040,6316,6592]
 
     print('Running LCS1 on generated data...')
@@ -66,7 +71,7 @@ if RUN_ALL:
 
     print('Running LCS2 on generated data...')
     for i in idx2:
-        S1 = dna_str(i) # generate a random ACTG string of length i.
+        S1 = dna_str(i)
         S2 = dna_str(i)
         LCS2(S1, S2)
     
@@ -75,6 +80,6 @@ if RUN_ALL:
         S1 = dna_str(i)
         S2 = dna_str(i)
         LCS3(S1, S2)
-  
-    print('-------------------------fin-------------------------------')
+    print()
+    print('--------------------------fin-------------------------------')
     print()
